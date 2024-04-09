345b67b501569aba7a8fb2891f0442804d231701
mohammad-karimi <99.mohammad.karimi@gmail.com>
2024-04-09 16:55:52 +0330

    fix:bug

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import * as THREE from 'three';
 import { useEffect, useRef } from 'react';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { materialBody, materialWheel, materialRotor, materialDoor, materialBody2 } from './colors';
 
 function App() {
@@ -31,14 +32,27 @@ function App() {
     const geometry = new THREE.BoxGeometry(1000, 1, 1000);
     const material = new THREE.MeshBasicMaterial({ color: 0x1e3e7f });
     const cube = new THREE.Mesh(geometry, material);
+
+    const geometry2 = new THREE.BoxGeometry(5, 5, 5);
+    const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const cube2 = new THREE.Mesh(geometry2, material2);
+
     cube.position.set(-100, -100, 0)
     scene.add(cube);
+    // scene.add(cube2);
 
+    console.log(window.devicePixelRatio, "pixel ratio");
 
+    const axesHelper = new THREE.AxesHelper(10)
+    scene.add(axesHelper)
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(1 * window.innerWidth, 1 * window.innerHeight);
     el.current.appendChild(renderer.domElement);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    const a = new THREE.Euler(0, 1, 0, 'XYZ');
 
     const loader = new GLTFLoader();
     var heli;
@@ -48,26 +62,28 @@ function App() {
 
       heli = gltf.scene;
       heli.scale.set(0.1, 0.1, 0.1); // Adjust the scale as needed
-      console.log(gltf);
+      // console.log(gltf);
       let children = heli.children[0].children
-      console.log(children);
-
+      // console.log(children);
+      heli.position.set(0, 0, 0)
       for (const i of children) {
-        // console.log(i.name)
+        console.log(i.name)
         if (i.name === "Body") {
           // console.log("yyyyyyyyyyyyy");
           i.material = materialBody
         } else if (i.name.includes("Wheel")) {
+          console.log("Applying material to wheel:", i.name);
           i.material = materialWheel;
         } else if (i.name.includes("Door")) {
           // console.log(i.name);
           i.material = materialDoor;
         } else if (i.name.includes("Rotor")) {
           // console.log(i);
+          i.position.set(0, 0, 0)
           i.material = materialRotor;
-        } else if (i.name.includes("Body_14")||i.name.includes(d"Body_8")) {
+        } else if (i.name.includes("Body_14") || i.name.includes("Body_8")) {
           // console.log(i);
-          i.material = materialBody2;
+          // i.material = materialBody2;
         } else {
           console.log(i.name);
         }
@@ -84,9 +100,29 @@ function App() {
 
     function animate() {
       requestAnimationFrame(animate);
-      if (heli)
-        heli.rotation.y += 0.002;
+      controls.update();
+      // if (heli)
+      // heli.rotation.y += 0.002;
+      if (heli && heli.children[0].children) {
+        // heli.rotation.y += 0.02
+        var center = new THREE.Vector3(0, 0, 0);
+        // heli.getCenter(center);
+        heli.position.sub(center);
+        for (const i of heli.children[0].children) {
+          if (i.name.includes("TopRotor")) {
+            // i.getCenter(i.position)
+            // heli.position.set(0, 0, 0)
+            console.log(i);
+            // i.position.sub(center);
+            // let offset = new THREE.Vector3(0, 5, 0);
+            // i.rotateOnWorldAxis(offset, 0.5)
 
+            // i.getCenter(offset);
+            i.rotation.y += 0.01
+            // i.setRotationFromMatrix(a);
+          }
+        }
+      }
 
       renderer.render(scene, camera);
     }
